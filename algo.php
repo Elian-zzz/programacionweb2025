fa2ff59cf66a2daace4fca35380a53b34cf03b33
Elian <u192168195@gmail.com>
2025-03-27 14:40:43 -0300

    trabajamos con variables

--- algo.php
+++ algo.php
@@ -8,4 +8,24 @@
 un comentario
 */
 echo "hola mundo";
+/*
+Las variables en php pueden ser:
+
+$nombre = "juan"; ==> texto, puede ser con comillas dobles o simples
+
+$edad = 25; ==> entero (Integer)
+
+$altura = 1.72 ; ==> decimal (Float)
+
+$es_estudiante = true; ==> booleano (Boolean)
+
+*/
+// $nombreCompleto = "Juan Pérez" | error: no cierra la variable
+$Edad=25; # todo good
+$estAtura=1.75; # todo good
+$numero="10"; # | error: no es un entero 
+$Apellido=`Fernandez` # todo good
+//  pais_de_origen="uruguay" | error: no comienza la variable 
+//  $salario = $2000; | error: la variable contiene caracteres no aceptados
 ?>
+
