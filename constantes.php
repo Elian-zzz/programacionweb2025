204185d8e8f807f8213f54afd2b2edbfac726bc6
Elian <u192168195@gmail.com>
2025-03-27 15:35:36 -0300

    constantes

---
+++ constantes.php
@@ -0,0 +1,7 @@
+<?php 
+const pi = 3.1416;
+pi = 3.14;
+// 1) error de sintaxys
+// 2) error de tipo de dato
+// 3) en calculos matematicos especificos como en constantes fisicas o astronomicas 
+?>
