5c376ca08ad5e90f88acb58d71f665d715fe0cbc
Elian <u192168195@gmail.com>
2025-03-27 14:04:47 -0300

    comentarios

--- algo.php
+++ algo.php
@@ -1,2 +1,11 @@
 <?php echo "hola mundo";?>1
 <?php echo "otro echo mas juaaaajajaj";?>
+<?= "que pasaría si?? ";?>
+<?php  
+// me come lo coco 
+# if o else
+/*
+un comentario
+*/
+echo "hola mundo";
+?>
