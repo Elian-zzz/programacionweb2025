d6e6fc5c64e780dfb0694367b984399756a34371
Elian <u192168195@gmail.com>
2025-05-15 15:37:06 -0300

    conversor finalizado

--- Practico monedas/conversor.php
+++ Practico monedas/conversor.php
@@ -1,9 +1,12 @@
 <?php
-const dolar = 40;
-const euro = 43;
-const realB = 8;
-
-
+// const dolar = 40;
+// const euro = 43;
+// const realB = 8;
+$monedas = [40, 43, 8]; // reemplazamos las constantes con arreglos
+var_dump($monedas); // para ver el contenido del array 
+// Para agregar un elemento al ultimo lugar libre de un array es de la siguiente manera
+// $monedas[] = 100; // agrega un elemento al final del array
+// Para agregar un elemento en una posicion especifica del array se hace de la siguiente manera
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") { // no funciona el conector,  o al menos no conecta con index.html
 
@@ -12,21 +15,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { // no funciona el conector,  o al me
 
     switch ($seleccionado) {
         case 'USD':
-            $resultado = floatval(value: $pesos / dolar);
+            $resultado = floatval(value: $pesos / $monedas[0]);
             $resultado_parseado = number_format($resultado, 2, ".");
-            echo "resultado de conversión de $$pesos a dolares es: $resultado_parseado";
+            echo "resultado de conversión de UY$$pesos a dolares es: US$$resultado_parseado";
             break;
         case 'EUR':
-            $resultado = floatval(value: $pesos / euro);
+            $resultado = floatval(value: $pesos / $monedas[1]);
             $resultado_parseado = number_format($resultado, 2, ".");
 
-            echo "resultado de conversión de $pesos a euros es: $resultado_parseado";
+            echo "resultado de conversión de UY$$pesos a euros es: $resultado_parseado €";
             break;
         case 'BRL':
-            $resultado = floatval(value: $pesos / realB);
+            $resultado = floatval(value: $pesos / $monedas[2]);
             $resultado_parseado = number_format($resultado, 2, ".");
 
-            echo "resultado de conversión de $pesos a reales es: $resultado_parseado";
+            echo "resultado de conversión de UY$$pesos a reales es: $resultado_parseado R$";
             break;
         default:
             echo "valores no asociados";
