1386e242f1bd20ccd1e5992d595e5bdf6d86a1ae
Elian <u192168195@gmail.com>
2025-05-15 15:03:31 -0300

    cambios

--- Practico monedas/conversor.php
+++ Practico monedas/conversor.php
@@ -1,5 +1,36 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $seleccionado = $_POST["select-monedas"];
-    $monto = intval($_POST["importe"]);
+const dolar = 40;
+const euro = 43;
+const realB = 8;
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") { // no funciona el conector,  o al menos no conecta con index.html
+
+    $seleccionado = strval($_POST["select-monedas"]);
+    $pesos = intval(value: $_POST["importe"]);
+
+    switch ($seleccionado) {
+        case 'USD':
+            $resultado = floatval(value: $pesos / dolar);
+            $resultado_parseado = number_format($resultado, 2, ".");
+            echo "resultado de conversión de $$pesos a dolares es: $resultado_parseado";
+            break;
+        case 'EUR':
+            $resultado = floatval(value: $pesos / euro);
+            $resultado_parseado = number_format($resultado, 2, ".");
+
+            echo "resultado de conversión de $pesos a euros es: $resultado_parseado";
+            break;
+        case 'BRL':
+            $resultado = floatval(value: $pesos / realB);
+            $resultado_parseado = number_format($resultado, 2, ".");
+
+            echo "resultado de conversión de $pesos a reales es: $resultado_parseado";
+            break;
+        default:
+            echo "valores no asociados";
+            break;
+            // }
+    }
 }
